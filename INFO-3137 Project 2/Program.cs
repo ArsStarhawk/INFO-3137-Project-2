684f64e8ff8638795b66d8130a5184608d9613c8
James Kidd <garlicraspberry@hotmail.com>
2020-07-19 03:25:12 -0400

    UI almost done?

--- INFO-3137 Project 2/Program.cs
+++ INFO-3137 Project 2/Program.cs
@@ -11,28 +11,107 @@ namespace INFO_3137_Project_2
     {
         static void Main(string[] args)
         {
-            Director director = new Director("JSON");
-            
-            director._name = "branch1";
-            director.BuildBranch();
+            string input = "";
+            string selection = "";
+            Director director = null;
 
-            director._name = "leaf1";
-            director._content = "content";
-            director.BuildLeaf();
-director.CloseBranch();
-            director._name = "leaf2";
-            director._content = "content";
-            director.BuildLeaf();
-
-          
-
-            director.Print();
-
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
+            UIHelper.PrintUsage();
             Console.WriteLine();
 
+            do
+            {
+                Console.Write('>');
+                input = Console.ReadLine();
+
+                selection = input.Contains(':') ? input.Remove(input.IndexOf(':')).ToUpper() : input.ToUpper();
+
+                switch (selection)
+                {
+                    case "HELP":
+                        UIHelper.PrintUsage();
+                        break;
+
+                    case "MODE":
+                        if (input.  Substring(input.IndexOf(':')+1).ToUpper().Equals("JSON"))
+                        {
+                            director = new Director("JSON");
+                        }
+                        else if ((input.Substring(input.IndexOf(':')+1).ToUpper().Equals("XML")))
+                        {
+                            director = new Director("XML");
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nInvalid input. For Usage, type 'Help'\n");
+                        }
+                        break;
+
+                    case "BRANCH":
+                        
+                        if(director == null) 
+                        {
+                            Console.WriteLine("Error. Mode has not been set. For usage, type 'Help'");
+                            break;
+                        }
+
+                        director._name = input.Substring(input.IndexOf(':') + 1);
+                        director.BuildBranch();
+                        break;
+
+                    case "LEAF":
+                        if (director == null)
+                        {
+                            Console.WriteLine("Error. Mode has not been set. For usage, type 'Help'");
+                            break;
+                        }
+
+                        if (input.Count(x => x == ':') != 2)
+                        {
+                            Console.WriteLine("Invalid input. For Usage, type 'Help'");
+                            break;
+                        }
+
+                        int firstDelim = input.IndexOf(':') + 1;
+                        int secondDelim = input.LastIndexOf(':'); 
+
+                        string name = input.Substring(firstDelim, secondDelim - firstDelim);
+                        string content = input.Substring(secondDelim +1);
+
+
+
+                        director._name = name;
+                        director._content = content;
+                        director.BuildLeaf();
+                        break;
+
+                    case "CLOSE":
+                        if (director == null)
+                        {
+                            Console.WriteLine("Error. Mode has not been set. For usage, type 'Help'");
+                            break;
+                        }
+
+                        director.CloseBranch();
+                        break;
+
+                    case "PRINT":
+                        if (director == null)
+                        {
+                            Console.WriteLine("Error. Mode has not been set. For usage, type 'Help'");
+                            break;
+                        }
+
+                        director.Print();
+                        break;
+
+                    case "EXIT":
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid input. For Usage, type 'Help'");
+                        break;
+                }
+            } while (!selection.Equals("EXIT"));
         }
     }
 }
